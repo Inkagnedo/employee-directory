e6a71a1357c0e6b3a0afd823a8ef7331b0880ffb
Inkagnedo <CCelestin44@gmail.com>
2020-05-11 14:01:27 -0400

    added build & got it to run locally

---
+++ src/components/TableRow/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+
+function TableRow(props) {
+    return (
+        <tr>
+            <td>{props.id}</td>
+            <td><Image src={props.photo} alt="face" roundedCircle></Image></td>
+            <td>{props.name}</td>
+            <td>{props.email}</td>
+        </tr>
+    );
+}
+
+export default TableRow;
